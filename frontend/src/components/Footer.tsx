6a0a514f7d680151d6eada0a1b02e48f47cf4373
Harikant Bajaj <harikantb08@gmail.com>
2024-06-02 15:38:52 +0530

    Check.IN

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div className="bg-gray-800 py-10">
+      <div className="container mx-auto flex justify-between items-center">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          Check.IN
+        </span>
+        <span className="text-white font-bold tracking-tight flex gap-4">
+          <p className="cursor-pointer">Profile</p>
+          <p className="cursor-pointer">Booking</p>
+          <p className="cursor-pointer">Payment</p>
+          <p className="cursor-pointer">Privacy Policy</p>
+          <p className="cursor-pointer">Terms of Service</p>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <div className="bg-gray-900 py-10">
+      <div className="container mx-auto flex flex-col gap-4">
+        <h1 className="text-5xl text-white font-bold hover:text-gray-500" >Find your luxury stay</h1>
+        <p className="text-2xl text-white hover:text-gray-500">
+           Hotels for your dream vacation...
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
